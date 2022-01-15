d4708ec0bc48b5adb62dfe8e2bc27436df90af4b
sakamuuy <sakamuu.y@gmail.com>
2022-01-15 11:51:14 +0900

    Read jokes from db'

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,4 +1,5 @@
-import { LinksFunction, Outlet, Link } from 'remix'
+import { LinksFunction, Outlet, Link, LoaderFunction, useLoaderData } from 'remix'
+import { db } from '~/utils/db.server'
 import stylesUrl from '../styles/jokes.css'
 
 export const links: LinksFunction = () => {
@@ -10,7 +11,19 @@ export const links: LinksFunction = () => {
   ]
 }
 
+type LoaderData = {
+  jokeListItems: Array<{ id: string; name: string }>;
+}
+
+export const loader: LoaderFunction = async () => {
+  const data: LoaderData = {
+    jokeListItems: await db.joke.findMany()
+  }
+  return data
+}
+
 export default function JokesRoute() {
+  const data = useLoaderData<LoaderData>()
   return (
     <div className="jokes-layout">
       <header className="jokes-header">
@@ -33,9 +46,11 @@ export default function JokesRoute() {
             <Link to=".">Get a random joke</Link>
             <p>Here are a few more jokes to check out:</p>
             <ul>
-              <li>
-                <Link to="some-joke-id">Hippo</Link>
-              </li>
+              {data.jokeListItems.map((joke) => (
+                <li key={joke.id}>
+                  <Link to={joke.id}>{joke.name}</Link>
+                </li>
+              ))}
             </ul>
             <Link to="new" className="button">
               Add your own
